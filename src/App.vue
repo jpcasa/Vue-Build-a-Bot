6fc9d32a4b309b90e4acfd6a35612642dcc8b634
Juan Pablo Casabianca <jpc@gcisas.com>
2018-07-18 12:13:36 -0500

    Vuex Connection

--- src/App.vue
+++ src/App.vue
@@ -4,47 +4,69 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <img class="logo" src="./assets/build-a-bot-logo.png">
-            Build-a-Bot
+            <router-link class="nav-link" :to="{ name: 'Home', params: {} }" exact>
+              <img class="logo" src="./assets/build-a-bot-logo.png">
+              Build-a-Bot
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'Build', params: {} }" exact>
+              Builb
+            </router-link>
+          </li>
+          <li class="nav-item cart">
+            <router-link class="nav-link" to="/cart" exact>
+              Cart
+            </router-link>
+            <div class="cart-items">
+              {{cart.length}}
+            </div>
           </li>
         </ul>
       </nav>
     </header>
-    <main>
-      <HomePage />
-    </main>
+    <div class="container">
+      <aside class="aside">
+        <router-view name="sidebar"/>
+      </aside>
+      <main>
+        <router-view/>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
-
 export default {
   name: 'app',
-  components: {
-    HomePage,
+  computed: {
+    cart() {
+      return this.$store.state.cart;
+    },
   },
 };
 </script>
 
 <style>
-body {
-  background: linear-gradient(to bottom, #555, #999);
-  background-attachment: fixed;
-}
+  body {
+    background: linear-gradient(to bottom, #555, #999);
+    background-attachment: fixed;
+  }
+</style>
+
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
 }
 main {
-  margin: 0 auto;
   padding: 30px;
   background-color: white;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -57,8 +79,43 @@ ul {
   font-size: 22px;
   border-right: 1px solid #bbb;
 }
+.nav-item.cart {
+  position: relative;
+  margin-left: auto;
+  border-right: none;
+}
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active {
+  color: #fff;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  padding: 30px;
+  background-color: #aaa;
+  width: 100px;
+  min-height: 300px;
+}
+.cart-items {
+  position: absolute;
+  top: -5px;
+  right: -9px;
+  font-size: 15px;
+  width: 20px;
+  text-align: center;
+  display: inline-block;
+  border-radius: 100px;
+  background-color: mediumseagreen;
+  color: #fff;
 }
 </style>
---
+++ src/parts/BrowseParts.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Browse Parts</h1>
+    <ul class="menu">
+      <li>
+        <router-link :to="{ name: 'BrowseHeads', params: {} }">Heads</router-link>
+      </li>
+      <router-link :to="{ name: 'BrowseArms', params: {} }">Arms</router-link>
+      <router-link :to="{ name: 'BrowseTorsos', params: {} }">Torsos</router-link>
+      <router-link :to="{ name: 'BrowseBases', params: {} }">Bases</router-link>
+    </ul>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BrowseParts',
+};
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+  }
+  .menu {
+    display: flex;
+    justify-content: space-around;
+    margin: 0 100px;
+    font-size: 20px;
+  }
+  ul {
+    list-style-type: none;
+  }
+  a {
+    color: palevioletred;
+  }
+</style>
